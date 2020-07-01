beb06b2c048571da18de95d642dc8f9effb05d63
agungTuanany <agung.tuanany@gmail.com>
2020-07-01 18:59:48 +0700

    Submit collections with form into mongoDB

--- imports/ui/App.svelte
+++ imports/ui/App.svelte
@@ -1,58 +1,96 @@
 <!-- Implement Svelte Component -->
 <script>
-    import { useTracker } from 'meteor/rdb:svelte-meteor-data'
+    import { useTracker } from 'meteor/rdb:svelte-meteor-data';
     import { Issues } from "../api/issues.js"
     import Issue from './Issue.svelte'
 
     // Reactive Svelte component
     $: issues = useTracker(() => Issues.find({}).fetch())
+
+    let newIssue = {
+        title       : "",
+        description : "",
+        dueDate     : "",
+        priority    : "low"
+
+    }
 /*
 function getIssues() {
     return [
         // Dummy object
-        { title       : "First Title",
+        { title         : "First Title",
             description : "First Issue Description",
             dueDate     : "2021-02-12",
             priority    : "low"
         },
-        { title       : "Second Title",
+        { title         : "Second Title",
             description : "Second Issue Description",
             dueDate     : "2021-02-12",
             priority    : "normal"
         },
-        { title       : 'Third Title',
+        { title         : 'Third Title',
             description : "Third Issue Description",
             dueDate     : "2021-02-12",
             priority    : "High"
         },
     ]
 }
-*/
+ */
+
+function handleSubmit(event) {
+    Issues.insert({
+        title       : newIssue.title,
+        description : newIssue.description,
+        dueDate     : newIssue.dueDate,
+        priority    : newIssue.priority
+    })
+
+    newIssue = {
+        title       : "",
+        description : "",
+        dueDate     : "",
+        priority    : "low"
+    }
+}
+
 
 </script>
 
 
 <div>
     <header>
-        <h1>
-            Issues:
-        </h1>
+        <h1>Issues:</h1>
+        <form on:submit|preventDefault={handleSubmit}>
+            <label for="title">Title</label>
+            <input id="title" type="text" bind:value={newIssue.title}/>
+            <label for="description">Description:</label>
+            <input id="title" type="text" bind:value={newIssue.description}/>
+            <label for="dueDate">Due Date</label>
+            <input id="title" type="text" bind:value={newIssue.dueDate}/>
+            <label for="priority">Priority:</label>
+            <select id="title" type="text" bind:value={newIssue.priority}>
+                <option value="low">Low</option>
+                <option value="medium">Medium</option>
+                <option value="high">High</option>
+            </select>
+            <input type="submit"/>
+        </form>
     </header>
     {#each $issues as issue}
         <!--
-        <ul>
+            <ul>
             <Issue title={ issue.title }
-                description={ issue.description }
-                dueDate={ issue.dueDate }
-                priority={ issue.priority }
+            description={ issue.description }
+            dueDate={ issue.dueDate }
+            priority={ issue.priority }
             />
-        </ul>
+            </ul>
         -->
 
         <ul>
             <Issue
                 issue={ issue }
-            />
+                />
 
         </ul>
 
