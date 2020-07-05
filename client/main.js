a4f01853bdd6933715b270be2717a18ac6634e31
agungTuanany <agung.tuanany@gmail.com>
2020-07-06 03:01:38 +0700

    Add Reactive svelte component rules only login user can create an issue

--- client/main.js
+++ client/main.js
@@ -1,6 +1,7 @@
 // Dependecies
 import { Meteor } from 'meteor/meteor'
 import App from "../imports/ui/App.svelte"
+import "../imports/startup/accounts-config.js"
 
 
 
--- imports/ui/App.svelte
+++ imports/ui/App.svelte
@@ -1,12 +1,14 @@
 <!-- Implement Svelte Component -->
 <script>
-    import { useTracker }    from 'meteor/rdb:svelte-meteor-data';
+    import { Meteor }        from "meteor/meteor"
+    import { useTracker }    from "meteor/rdb:svelte-meteor-data";
     import { BlazeTemplate } from "meteor/svelte:blaze-integration"
     import { Issues }        from "../api/issues.js"
-    import Issue             from './Issue.svelte'
+    import Issue             from "./Issue.svelte"
 
     // Reactive Svelte component
-    $: issues = useTracker(() => Issues.find({}).fetch())
+    $: issues      = useTracker(() => Issues.find({}).fetch())
+    $: currentUser = useTracker(() => Meteor.user())
 
     let newIssue = {
         title       : "",
@@ -43,7 +45,9 @@ function handleSubmit(event) {
         title       : newIssue.title,
         description : newIssue.description,
         dueDate     : newIssue.dueDate,
-        priority    : newIssue.priority
+        priority    : newIssue.priority,
+        userId      : Meteor.userId(),
+        userName    : Meteor.user().username
     })
 
     newIssue = {
@@ -57,6 +61,16 @@ function handleSubmit(event) {
 
 </script>
 
+<style>
+    .head_title {
+        margin-top: 110px;
+    }
+
+    /* .form_issues { */
+    /*     padding-top: 75px; */
+    /* } */
+</style>
+
 
 <div>
     <header>
@@ -67,24 +81,26 @@ function handleSubmit(event) {
             </div>
         </nav>
 
-        <h1>Issues:</h1>
+        <h1 class="head_title">Issues:</h1>
 
+        {#if $currentUser}
+            <form on:submit|preventDefault={handleSubmit}>
+                <label for="title">Title</label>
+                <input id="title" type="text" bind:value={newIssue.title}/>
+                <label for="description">Description:</label>
+                <input id="title" type="text" bind:value={newIssue.description}/>
+                <label for="dueDate">Due Date</label>
+                <input id="title" type="text" bind:value={newIssue.dueDate}/>
+                <label for="priority">Priority:</label>
+                <select id="title" type="text" bind:value={newIssue.priority}>
+                    <option value="low">Low</option>
+                    <option value="medium">Medium</option>
+                    <option value="high">High</option>
+                </select>
+                <input type="submit"/>
+            </form>
+        {/if}
 
-        <form on:submit|preventDefault={handleSubmit}>
-            <label for="title">Title</label>
-            <input id="title" type="text" bind:value={newIssue.title}/>
-            <label for="description">Description:</label>
-            <input id="title" type="text" bind:value={newIssue.description}/>
-            <label for="dueDate">Due Date</label>
-            <input id="title" type="text" bind:value={newIssue.dueDate}/>
-            <label for="priority">Priority:</label>
-            <select id="title" type="text" bind:value={newIssue.priority}>
-                <option value="low">Low</option>
-                <option value="medium">Medium</option>
-                <option value="high">High</option>
-            </select>
-            <input type="submit"/>
-        </form>
     </header>
     <div>
             <!--
